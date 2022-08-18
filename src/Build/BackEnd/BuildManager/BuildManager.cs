5974eac096b5529f01d5e796f37bd11275ce43bd
Pawan Kartik <85254170+pawan-deepsource@users.noreply.github.com>
2022-08-18 17:37:21 +0530

    Update BuildManager.cs

--- src/Build/BackEnd/BuildManager/BuildManager.cs
+++ src/Build/BackEnd/BuildManager/BuildManager.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+// Test comment
+
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
